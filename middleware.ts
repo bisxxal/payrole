e24a9771dacead17d2abf0d115b343e02929e802
bisxxal <bishalkandi859494@gmail.com>
2025-04-21 21:54:50 +0530

    init

--- middleware.ts
+++ middleware.ts
@@ -1,48 +1,4 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
  
-// import { NextResponse } from "next/server";
-// // import { routeAccessMap } from "./lib/procatedRoutes";
-
-
-// type RouteAccessMap = {
-//     [key: string]: string[];
-//   };
-  
-//   export const routeAccessMap: RouteAccessMap = {
-//     "/admin(.*)": ["admin"],
-//     "/emp(.*)": ["emp"], 
-//   };
-
-// const matchers = Object.keys(routeAccessMap).map((route) => ({
-//   matcher: createRouteMatcher([route]),
-//   allowedRoles: routeAccessMap[route],
-// }));
- 
-
-// export default clerkMiddleware( async (auth, req) => { 
-
-//   const { sessionClaims } =await auth();
-
-// //   const role = (sessionClaims?.metadata as { role?: string })?.role;
-// const role = (sessionClaims?.metadata as { role?: string })?.role;
-
-//   for (const { matcher, allowedRoles } of matchers) {
-//     if (matcher(req) && !allowedRoles.includes(role!)) {
-//       return NextResponse.redirect(new URL(`/${role}`, req.url));
-//     }
-//   }
-// });
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// }; 
-
-
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware()
